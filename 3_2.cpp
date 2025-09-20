a5503fe6e170a0550d53442de1094defc5680de7
solgeo73 <sol.geo.200773@gmail.com>
2025-09-20 11:10:19 +0300

    Create 3_2.cpp

---
+++ 3_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    int c = 0;
+    int m = INT_MIN;
+    for (int k=0; k < n; k++) {
+        cin >> a[k];
+        if (a[k] % 2 == 0) {
+            c++;
+        }
+        else if (a[k] > m) {
+            m = a[k];
+        }
+    }
+    cout << m << endl << c << endl;
+    return 0;
+}
