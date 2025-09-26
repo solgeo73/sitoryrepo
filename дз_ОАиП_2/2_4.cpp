d6dea217784e59959ec343eaa2d9d57cddf7517e
solgeo73 <sol.geo.200773@gmail.com>
2025-09-26 09:31:34 +0300

    Create 2_4.cpp

---
+++ дз_ОАиП_2/2_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int c;
+    cin >> c;
+    if (c % 2 == 0) {
+        cout << "Чётное";
+    }
+    else {
+        cout << "Нечётное";
+    }
+    return 0;
+}
