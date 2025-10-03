fb0c880a7fcc75fac560d137a77ed5c7bc61cfd2
solgeo73 <sol.geo.200773@gmail.com>
2025-10-03 09:30:05 +0300

    Create 3g_1.cpp

---
+++ дз_ОАиП_3_геом/3g_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a[3][2];
+    for (int k=0; k < 3; k++) {
+        cin >> a[k][0] >> a[k][1];
+    }
+    cout << 0.5 * abs(a[0][0]*(a[1][1]-a[2][1]) + a[1][0]*(a[2][1]-a[0][1]) + a[2][0]*(a[0][1]-a[1][1]));
+    return 0;
+}
