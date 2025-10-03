c1fe9cf9f2192bd9912d8e87c424f54f6acf3077
solgeo73 <sol.geo.200773@gmail.com>
2025-10-03 10:31:29 +0300

    Create 3g_4.cpp

---
+++ дз_ОАиП_3_геом/3g_4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a[4][2];
+    int x, y;
+    for (int k=0; k < 4; k++) {
+        cin >> x >> y;
+        a[k][0] = x;
+        a[k][1] = y;
+    }
+    bool f=0;
+    for (int k=0; k < 4; k++) {
+        for (int q=0; q < 4; q++) {
+            if (a[k][0]!=a[q][0] && a[k][1]!=a[q][1]) {
+                cout << sqrt(pow(a[q][0]-a[k][0], 2) + pow(a[q][1]-a[k][1], 2));
+                f=1;
+                break;
+            }
+        }
+        if (f) {
+            break;
+        }
+    }
+    return 0;
+}
