544541a2f0898ae356cda68caf3d5622a2202825
solgeo73 <sol.geo.200773@gmail.com>
2025-10-18 11:30:39 +0300

    Update лаб_оаип_2.cpp

--- лаб_оаип_2.cpp
+++ лаб_оаип_2.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 using namespace std;
 
 int main() {
     char str[100];
-    char a[20];
+    vector <char> a;
     cin.getline(str, 100);
     char *p1 = str;
-    short cb, ln, mx, pr, d;
-    cb = ln = mx = pr = d = 0;
+    short cb, ln, mx, pr, d, i, c2;
+    cb = ln = mx = pr = d = i = c2 = 0;
     bool f = false;
     const char *w = " ";
     strncat(str, w, 1);
@@ -16,20 +17,29 @@ int main() {
         if (pr == 9) { if (*p1 == 'd') { d++; } }
         if (*p1 != ' ') { ln++; }
         else {if (ln > mx) { mx = ln; } ln = 0; pr++; }
+        if (ln == 2) { a.insert(a.begin(), *p1); }
         if (*p1 == ' ' && f) { cb++; }
         if (*p1 == 'b') { f = true; }
         else { f = false; }
         p1++;
     }
+    ln = 0;
+    while (p1 != &str[0]) {
+        if (*p1 != ' ') { ln++; }
+        else { ln = 0; }
+        if (ln == 2) { if (*p1 == a[i]) { c2++; } i++; }
+        p1--;
+    }
     cout << "1. Количество слов, оканчивающихся на букву b: " << cb << endl;
     cout << "2. Длина самого длинного слова: " << mx << endl;
     cout << "3. Количество букв d в последнем слове строки: " << d << endl;
     cout << "4. Строка из заглавных букв:\n";
-    char *p2 = str;
-    while (*p2 != '\0') {
-        *p2 = toupper(*p2);
-        cout << *p2;
-        p2++;
+    p1 = &str[0];
+    while (*p1 != '\0') {
+        *p1 = toupper(*p1);
+        cout << *p1;
+        p1++;
     }
+    cout << "\n5. Количество слов, у которых совпадает второй и предпоследний символ: " << c2; 
     return 0;
 }
