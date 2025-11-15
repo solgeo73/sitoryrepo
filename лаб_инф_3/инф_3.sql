47fca7795d6a259e6816e0303a53ecd7e01ca79f
solgeo73 <sol.geo.200773@gmail.com>
2025-11-15 09:25:55 +0300

    Update инф_3.sql

--- лаб_инф_3/инф_3.sql
+++ лаб_инф_3/инф_3.sql
@@ -19,20 +19,23 @@ CREATE TABLE orders (
  total_price INT NOT NULL
 );
 INSERT INTO categories(c_id, category_name)
-VALUES ((1, 'Супы'),
+VALUES (1, 'Супы'),
 (2, 'Основные блюда'),
 (3, 'Закуски'),
-(4, 'Десерты'));
+(4, 'Десерты');
 INSERT INTO dishes(d_id, d_name, category_id, price)
-VALUES ((1, 'Борщ', 1, 500),
+VALUES (1, 'Борщ', 1, 500),
 (2, 'Солянка', 1, 450),
 (3, 'Гуляш', 2, 550),
-(4, 'Торт', 4, 350));
+(4, 'Торт', 4, 350);
 INSERT INTO orders(order_id, dish_id, order_date, quantity, total_price)
-VALUES ((1, 1, 2025-11-16, 2, price * quantity),
-(2, 3, 2025-11-16, 1, price * quantity),
-(3, 1, 2025-11-19, 1, price * quantity));
-SELECT * FROM dishes WHERE category_id = 1;
-SELECT * FROM orders WHERE order_date < 2025-11-20 and order_date > 2025-11-15;
-SELECT sum(total_price) FROM orders WHERE order_date = 2025-11-16;
-SELECT * FROM orders WHERE max(quantity);
+VALUES (1, 1, 2025-11-16, 2, 1000),
+(2, 3, 2025-11-16, 1, 550),
+(3, 1, 2025-11-19, 1, 500);
+SELECT d_name FROM dishes WHERE category_id = 2;
+SELECT * FROM orders WHERE order_date >= '2025-11-16' AND order_date <= '2025-11-18';
+SELECT SUM(total_price) as total_sum FROM orders WHERE order_date = '2025-11-16';
+SELECT dishes.d_name, SUM(orders.quantity) as total_ordered FROM orders
+JOIN dishes ON orders.dish_id = dishes.d_id
+GROUP BY dishes.d_id, dishes.d_name
+ORDER BY total_ordered DESC;
